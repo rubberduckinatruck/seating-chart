6b2a78b692cf39c7f600377310663a79968c5ffa
rubberduckinatruck <turnbull.gen@gmail.com>
2025-09-14 14:23:21 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/seating-chart/'
+  // Important: set base so GH Pages serves from /seating-chart/
+  base: '/seating-chart/',
 })
